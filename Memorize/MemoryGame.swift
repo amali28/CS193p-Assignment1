223fe6e4e88df889cb5e542d327d97836f9412ac
Ali Malik <33435221+amali28@users.noreply.github.com>
2021-12-11 17:18:17 -0500

    First commit
    First commit

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -5,4 +5,31 @@
 //  Created by Ali Malik on 2021-12-08.
 //
 
+// Model
+
 import Foundation
+
+struct MemoryGame <CardContent>{
+    var cards: Array<Card>
+    
+    func choose (_ card: Card){
+        
+    }
+    
+    init(numberOfPairsCards: Int, createCardContent: (Int) -> CardContent ){
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsCards{
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+        
+    }
+    
+    struct Card {
+        var isFaceup = false
+        var isMatched = false
+        var content: CardContent
+    }
+}
