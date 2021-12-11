acdd4911eab9fbde2ad3fda0c3e4e345990d2eb2
Ali Malik <amali28@uwo.ca>
2021-12-11 17:14:32 -0500

    first commit

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Ali Malik on 2021-12-08.
+//
+
+import Foundation
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Ali Malik on 2021-12-08.
+//
+
+import Foundation
